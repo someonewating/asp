cb9fb388723dacabee8992b5d26dd472340806fc
someonewating <someonewating@outlook.com>
2023-02-01 16:05:50 +0000

    Haven't tested since thingsboard down.

--- publish/send.py
+++ publish/send.py
@@ -4,26 +4,37 @@ import adafruit_dht
 from mqtt import MQTT
 import json
 import RPi.GPIO as GPIO
+import numpy as np
 
+#DHT11 sensor
 tem_hum_device = adafruit_dht.DHT11(board.D4)
+
+#GPS
+LATITUDE = 40.388
+LONGITUDE = -3.62834
 
 # MQTT
 broker = "srv-iot.diatel.upm.es"
 port = 8883
 topic = "v1/devices/me/telemetry"
 
-temp_hum_mqtt = MQTT(broker, port, topic, "bodyTem", "JCGf3ApcTWWLqul1Btxo")
-temp_hum_mqtt.connect()
-temp_hum_mqtt.start()
+gateway = MQTT(broker, port, topic, "sheep001", "7kDEhwF0RUtckoILFPeR")
+gateway.connect()
+gateway.start()
 
 while True:
     try:
+        heartbeat = np.random.normal(70, 5)
         temperature = tem_hum_device.temperature
 
         temp_hum_msg = json.dumps({
-            "temperature": temperature
+            "sheepid": 1,
+            "temperature": temperature,
+            "latitude": LATITUDE,
+            "longitude": LONGITUDE,
+            "heartbeat": heartbeat
         })
-        result = temp_hum_mqtt.publish(temp_hum_msg)
+        gateway.publish(temp_hum_msg)
 
     except KeyboardInterrupt:
         tem_hum_device.exit()
